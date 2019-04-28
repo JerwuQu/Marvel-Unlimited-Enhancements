4b9285e1c67fb9a0d5e2d423e39da3a0fa94eb9f
Marcus Ramse <marcus@ramse.se>
2019-04-28 15:05:22 +0200

    Update mue.js

--- mue.js
+++ mue.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Marvel Unlimited Enhancements
 // @namespace    https://ramse.se/
-// @version      0.3
+// @version      0.4
 // @description  Better UX
 // @author       Marcus Ramse
 // @match        https://read.marvel.com/
@@ -13,7 +13,11 @@
 // Style
 GM_addStyle(`
 .mue-hidden {
-    display: none;
+    display: none !important;
+}
+
+.mue-nocursor {
+    cursor: none !important;
 }
 
 .mue-help {
@@ -40,6 +44,7 @@ var MU_KEYS = {
 
 var MUE_KEYS = {
     advance:    32,   // space
+    cursor:     67,   // c
     help:       72,   // h
     library:    76,   // l
     nextIssue:  78,   // n
@@ -53,16 +58,17 @@ mueHelp.innerHTML = `
     <h2>Default hotkeys</h2>
     <div><strong>Left</strong> - Previous page</div>
     <div><strong>Right</strong> - Next page</div>
-    <div><strong>S</strong> - Toggle smart mode</div>
-    <div><strong>Z</strong> - Toggle zoom</div>
-    <div><strong>F11</strong> - Toggle fullscreen</div>
+    <div><strong>S</strong> - Smart mode (toggle)</div>
+    <div><strong>Z</strong> - Zoom (toggle)</div>
+    <div><strong>F11</strong> - Fullscreen (toggle)</div>
     <br />
     <h2>MUE hotkeys</h2>
     <div><strong>Space</strong> - Next page</div>
-    <div><strong>H</strong> - Show this help</div>
+    <div><strong>C</strong> - Hide the cursor (toggle)</div>
+    <div><strong>H</strong> - Show this help (toggle)</div>
     <div><strong>L</strong> - Return to My Library</div>
     <div><strong>N</strong> - Next issue</div>
-    <div><strong>T</strong> - Disable default UI</div>
+    <div><strong>T</strong> - Disable default UI (toggle)</div>
 </div>
 `;
 document.body.appendChild(mueHelp);
@@ -115,6 +121,10 @@ document.body.addEventListener('keydown', function(e) {
         case MUE_KEYS.advance:
             simulateKey(MU_KEYS.right);
             break;
+        case MUE_KEYS.cursor:
+            var body = document.body;
+            body.className = toggleClass(body.className, 'mue-nocursor');
+            break;
         case MUE_KEYS.help:
             var help = document.querySelector('.mue-help');
             help.className = toggleClass(help.className, 'mue-hidden');
@@ -123,8 +133,10 @@ document.body.addEventListener('keydown', function(e) {
             window.location.href = 'https://www.marvel.com/my_account/my_must_reads';
             break;
         case MUE_KEYS.nextIssue:
+            // todo: keep toggles
             window.location.href = 'https://read.marvel.com/#book/' +
                 document.querySelector('[data-nextid]').getAttribute('data-nextid');
+            location.reload();
             break;
         case MUE_KEYS.toggleUI:
             var header = document.querySelector('section#header');
